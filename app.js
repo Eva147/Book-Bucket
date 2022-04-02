5872c958c31b863070e7dd0481fff3cfa46608b6
Evgeniya <eva147@list.ru>
2022-04-02 15:27:24 +0100

    Add routes for get and post requests for new books and reviews

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Storing configuration in the environment separate from code
 require('dotenv').config();
 // require modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
 const URL = process.env.URL;
 
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 // use the public directory to store the static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 // connect mongoose to MongoDB
 main().catch(err => console.log(err));
 
@@ -30,16 +30,70 @@ async function main() {
     const bookReviewSchema = new mongoose.Schema({
         author: String,
         name: String,
+        rating: Number,
         content: String
     });
 
     const Book = mongoose.model('Book', bookListSchema);
     const Review = mongoose.model('Review', bookReviewSchema);
 
+    // /BOOKS route
+    app.get('/books', function(req, res){
+        Book.find(function(err, foundBooks){
+            if(!err){
+                res.send(foundBooks);
+            } else {
+                res.send(err);
+            }
+        });
+    });
+    app.post('/books', function(req, res){
+        // create a new book through the post request
+        const newBook = new Book ({
+            author: req.body.bookAuthor,
+            name: req.body.bookTitle
+        });
+        // save new book to mongoDB
+        newBook.save(function(err){
+            if (!err) {
+                res.send('A new book successfully added.')
+            } else {
+                res.send(err);
+            }
+        });
+    });
+    
 
+    // /REVIEWS route
+    app.get('/reviews', function(req, res){
+        Review.find(function(err, foundReviews){
+            if(!err){
+                res.send(foundReviews);
+            } else {
+                res.send(err);
+            }
+        });
+    });
+    app.post('/reviews', function(req, res){
+        // create a new book through the post request
+        const newReview = new Review ({
+            author: reviewBookAuthor,
+            name: reviewBookTitle,
+            rating: bookRating,
+            content: reviewContent
+        });
+        // save review to mongoDB
+        newReview.save(function(err){
+            if (!err) {
+                res.send('A new review successfully added.')
+            } else {
+                res.send(err);
+            }
+        });
+    });
 
 }
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log('Server started on port 3000');
 });
